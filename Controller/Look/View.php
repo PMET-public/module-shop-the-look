e30f07d3c2140e258fdf8708ae2ac454cc108e00
David Alger <david@classyllama.com>
2015-09-17 13:23:18 -0500

    Add shell ajax routes needed for view and item view

---
+++ Controller/Look/View.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MagentoEse\LookBook\Controller\Look;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\Controller\ResultFactory;
+
+class View extends Action
+{
+    public function execute()
+    {
+        /** @var \Magento\Framework\Controller\Result\Json $result */
+        $result = $this->resultFactory->create(ResultFactory::TYPE_JSON);
+        $result->setData([]);
+
+        return $result;
+    }
+}
