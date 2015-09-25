c992f36b13a5f2a09499892bdcec67c9466f78a8
David Alger <david@classyllama.com>
2015-09-25 15:46:52 -0500

    Setup AJAX reply with content on look/view controller

--- Controller/Look/View.php
+++ Controller/Look/View.php
@@ -2,16 +2,24 @@
 
 namespace MagentoEse\LookBook\Controller\Look;
 
-use Magento\Framework\App\Action\Action;
 use Magento\Framework\Controller\ResultFactory;
 
-class View extends Action
+class View extends \Magento\Catalog\Controller\Category\View
 {
     public function execute()
     {
-        /** @var \Magento\Framework\Controller\Result\Json $result */
         $result = $this->resultFactory->create(ResultFactory::TYPE_JSON);
-        $result->setData([]);
+        $data = [];
+
+        $category = $this->_initCategory();
+        if ($category) {
+            $page = $this->resultPageFactory->create();
+            $data['html'] = $page->getLayout()->getBlock('look.view')->toHtml();
+        } else {
+            $result->setHttpResponseCode(404);
+            $data['error'] = 1;
+        }
+        $result->setData($data);
 
         return $result;
     }
