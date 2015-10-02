b92ff93eae3f0a0b2255f491335ad057d50e943a
David Alger <david@classyllama.com>
2015-10-02 12:08:00 -0500

    Setup controller for item detail json

--- Controller/Look/Item.php
+++ Controller/Look/Item.php
@@ -2,17 +2,26 @@
 
 namespace MagentoEse\LookBook\Controller\Look;
 
-use Magento\Framework\App\Action\Action;
+use Magento\Catalog\Controller\Product\View;
 use Magento\Framework\Controller\ResultFactory;
 
-class Item extends Action
+class Item extends View
 {
     public function execute()
     {
         /** @var \Magento\Framework\Controller\Result\Json $result */
         $result = $this->resultFactory->create(ResultFactory::TYPE_JSON);
-        $result->setData([]);
+        $data = [];
 
+        $product = $this->_initProduct();
+        if ($product) {
+            $page = $this->resultPageFactory->create();
+            $data['html'] = $page->getLayout()->getBlock('lookbook.product.view')->toHtml();
+        } else {
+            $result->setHttpResponseCode(404);
+            $data['error'] = 1;
+        }
+        $result->setData($data);
         return $result;
     }
 }
