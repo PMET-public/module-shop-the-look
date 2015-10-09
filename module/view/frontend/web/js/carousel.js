1dcd08c441f46ba537e379d784bd075a42512237
David Alger <david@classyllama.com>
2015-10-09 16:10:19 -0500

    Updated styling to work with the new class names and page structure

--- module/view/frontend/web/js/carousel.js
+++ module/view/frontend/web/js/carousel.js
@@ -1,6 +1,7 @@
 define([
     'jquery',
-    'MagentoEse_LookBook/js/lib/owl.carousel'
+    'MagentoEse_LookBook/js/lib/owl.carousel',
+    '!domReady'
 ], function ($) {
     'use strict';
 
@@ -15,7 +16,37 @@ define([
             $(this.element); // wrap this.element
 
             this.element.addClass('owl-carousel');
-            this.element.owlCarousel(this.options);
+            var owl = this.element.owlCarousel(this.options);
+
+            this._redrawOnResize(owl);
+            this._addNavButtons(owl);
+            this._setKeyboardControlls(owl);
+        },
+
+        _setKeyboardControlls: function (owl) {
+            $(document).keyup(function(e){
+                if (e.keyCode == 37) {
+                    owl.trigger('prev.owl.carousel');
+                } else if (e.keyCode == 39) {
+                    owl.trigger('next.owl.carousel');
+                }
+            });
+        },
+        _redrawOnResize: function (owl) {
+            // After resizing window in Chrome, drawing issues cause other slides to show under current slide.
+            // Since there are no other apparent solutions, triggering a redraw is the best way of fixing this issue.
+            owl.on("resize.owl.carousel",function(e){
+                console.log("resize");
+                owl.hide().show(0);
+            });
+        },
+        _addNavButtons: function (owl) {
+            $('.column.main .prev').click(function(){
+                owl.trigger('prev.owl.carousel');
+            });
+            $('.column.main .next').click(function(){
+                owl.trigger('next.owl.carousel');
+            });
         }
     });
 
--- module/view/frontend/web/js/lookbook.js
+++ module/view/frontend/web/js/lookbook.js
@@ -1,10 +1,8 @@
 define([
     'jquery',
     'mage/translate',
-    'matchMedia',
-    'MagentoEse_LookBook/js/lib/owl.carousel',
     '!domReady'
-], function ($, owlCarousel){
+], function ($) {
     'use strict';
 
     $.widget('MagentoEse_LookBook.popup', {
@@ -15,45 +13,6 @@ define([
             responsive: true,
             innerScroll: true,
             buttons: []
-        },
-
-        /**
-         * Create MagentoEse_LookBook.popup widget
-         * @private
-         */
-        _create: function () {
-            var self = this;
-            var element = $(self.element);
-            element.addClass('owl-carousel');
-            var owl = $(self.element).owlCarousel(self.options);
-            self._redrawOnResize(owl);
-            self._addNavButtons(owl);
-            self._setKeyboardControlls(owl);
-        },
-        _setKeyboardControlls: function (owl) {
-            $(document).keyup(function(e){
-                if (e.keyCode == 37) {
-                    owl.trigger('prev.owl.carousel');
-                } else if (e.keyCode == 39) {
-                    owl.trigger('next.owl.carousel');
-                }
-            });
-        },
-        _redrawOnResize: function (owl) {
-            // After resizing window in Chrome, drawing issues cause other slides to show under current slide.
-            // Since there are no other apparent solutions, triggering a redraw is the best way of fixing this issue.
-            owl.on("resize.owl.carousel",function(e){
-                console.log("resize");
-                owl.hide().show(0);
-            });
-        },
-        _addNavButtons: function (owl) {
-            $('.column.main .prev').click(function(){
-                owl.trigger('prev.owl.carousel');
-            });
-            $('.column.main .next').click(function(){
-                owl.trigger('next.owl.carousel');
-            });
         }
     });
 
