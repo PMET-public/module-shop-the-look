8173a035d7aad7a916603071062a8163209fdfe3
Nathan Toombs <nathan.toombs@classyllama.com>
2015-10-09 14:47:59 -0500

    - Compleated styling of lookbook page - Compleated functionality of lookbook carousel

--- module/view/frontend/web/js/lookbook.js
+++ module/view/frontend/web/js/lookbook.js
@@ -1,5 +1,6 @@
 define([
     'jquery',
+    'matchMedia',
     'MagentoEse_LookBook/js/lib/owl.carousel',
     '!domReady'
 ], function ($, owlCarousel){
@@ -11,9 +12,40 @@ define([
          * @private
          */
         _create: function () {
-            $(this.element).addClass('owl-carousel');
-            $(this.element).owlCarousel(this.options);
+            var self = this;
+            var element = $(self.element);
+            element.addClass('owl-carousel');
+            var owl = $(self.element).owlCarousel(self.options);
+            self._redrawOnResize(owl);
+            self._addNavButtons(owl);
+            self._setKeyboardControlls(owl);
+        },
+        _setKeyboardControlls: function (owl) {
+            $(document).keyup(function(e){
+                if (e.keyCode == 37) {
+                    owl.trigger('prev.owl.carousel');
+                } else if (e.keyCode == 39) {
+                    owl.trigger('next.owl.carousel');
+                }
+            });
+        },
+        _redrawOnResize: function (owl) {
+            // After resizing window in Chrome, drawing issues cause other slides to show under current slide.
+            // Since there are no other apparent solutions, triggering a redraw is the best way of fixing this issue.
+            owl.on("resize.owl.carousel",function(e){
+                console.log("resize");
+                owl.hide().show(0);
+            });
+        },
+        _addNavButtons: function (owl) {
+            $('.column.main .prev').click(function(){
+                owl.trigger('prev.owl.carousel');
+            });
+            $('.column.main .next').click(function(){
+                owl.trigger('next.owl.carousel');
+            });
         }
+
     });
 
     return $.MagentoEse.lookBook;
