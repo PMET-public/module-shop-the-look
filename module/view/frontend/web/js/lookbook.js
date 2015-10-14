0d7b1c2fa573db5771fbd53a71ccca5c628dc39b
David Alger <david@classyllama.com>
2015-10-14 10:38:51 -0500

    Update JS to fix problems with it's new structure ;)

--- module/view/frontend/web/js/lookbook.js
+++ module/view/frontend/web/js/lookbook.js
@@ -10,6 +10,7 @@ define([
             this.options.loadUrl = $(event.toElement).data('load-url');
             this._super();
         },
+
         openModal: function () {
             if (this.options.loadUrl) {
                 this.openModal_super = this._super;
@@ -21,6 +22,7 @@ define([
                 }.bind(this))
             }
         },
+
         closeModal: function () {
             this._super();
             this.element.html('');
@@ -32,25 +34,23 @@ define([
             defer: true,
             listenEvent: null,
             loadUrl: null,
-            elementSelector: null,
-            activeProductId: 0,
+            elementSelector: null
         },
 
         _create: function () {
-            if (!this.options.defer) {
-                this._load()
-            }
 
             if (!this.options.loadUrl) {
                 this.options.loadUrl = this.element.data('load-url');
             }
 
-            if (this.options.listenEvent) {
-                this.element.on(this.options.listenEvent, this._load(self).bind(this))
+            this.options.defer = this.element.data('defer');
+            if (!this.options.defer) {
+                this._load()
             }
 
-            var self = this;
-            this._setActiveProductClass(self);
+            if (this.options.listenEvent) {
+                this.element.on(this.options.listenEvent, this._load.bind(this))
+            }
         },
 
         _load: function () {
@@ -59,27 +59,23 @@ define([
                 if (this.options.elementSelector) {
                     el = $(this.options.elementSelector);
                 }
+
                 if (this.options.defer) {
                     $(':mage-loader').loader('show');
                 }
 
                 el.load(this.options.loadUrl, function () {
                     $.mage.init();
-                    $('.product-item').removeClass('active');
-                    console.log(this.options.activeProductId);
-                    $('.product-item:eq('+this.options.activeProductId+')').addClass('active');
+
+                    $(':MagentoEse_LookBook-lookbookLoadDetail').removeClass('active');
+
                     if (this.options.defer) {
                         $(':mage-loader').loader('hide');
                     }
+
+                    this.element.addClass('active');
                 }.bind(this));
             }
-        },
-
-        _setActiveProductClass: function (self) {
-            $('._show .product-item').unbind('click');
-            $('._show .product-item').on('click', function () {
-                self.options.activeProductId = $('._show .product-item').index(this);
-            });
         }
     });
 
