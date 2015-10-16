1da43d22e75d3db42ede1f3313e6d56a161d6514
Nathan Toombs <nathan.toombs@classyllama.com>
2015-10-16 18:41:02 -0500

    - Updated templates on catalog.product.view - Added widget to extend catalogAddToCart and override ajax success method - Added addtocart template to override catalogAddToCart - Added gallery template to disable magnifier on lookbook view modal - Added styling to lookbook - Created lookbookAddToCart widget

--- module/view/frontend/requirejs-config.js
+++ module/view/frontend/requirejs-config.js
@@ -3,7 +3,8 @@ var config = {
         "*": {
             'MagentoEse_LookBook::lookbook': 'MagentoEse_LookBook/js/lookbook',
             'MagentoEse_LookBook::lookbookLoadDetail': 'MagentoEse_LookBook/js/lookbook',
-            'MagentoEse_LookBook::carousel': 'MagentoEse_LookBook/js/carousel'
+            'MagentoEse_LookBook::carousel': 'MagentoEse_LookBook/js/carousel',
+            lookbookAddToCart: 'MagentoEse_LookBook/js/lookbook-add-to-cart'
         }
     }
 };
---
+++ module/view/frontend/web/js/lookbook-add-to-cart.js
@@ -0,0 +1,69 @@
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+define([
+    'jquery',
+    'mage/translate',
+    'jquery/ui',
+    'Magento_Catalog/js/catalog-add-to-cart'
+], function($, $t) {
+    "use strict";
+
+    $.widget('MagentoEse.lookbookAddToCart', $.mage.catalogAddToCart, {
+        options: {
+            processStart: null,
+            processStop: null,
+            bindSubmit: true,
+            minicartSelector: '[data-block="minicart"]',
+            messagesSelector: '.lookbook .product-item.active',
+            productStatusSelector: '.stock.available',
+            addToCartButtonSelector: '.action.tocart',
+            addToCartButtonDisabledClass: 'disabled',
+            addToCartButtonTextWhileAdding: $t('Adding...'),
+            addToCartButtonTextAdded: $t('Added'),
+            addToCartButtonTextDefault: $t('Add to Cart')
+
+        },
+        ajaxSubmit: function(form) {
+            var self = this;
+            $(self.options.minicartSelector).trigger('contentLoading');
+            self.disableAddToCartButton(form);
+
+            $.ajax({
+                url: form.attr('action'),
+                data: form.serialize(),
+                type: 'post',
+                dataType: 'json',
+                beforeSend: function() {
+                    if (self.isLoaderEnabled()) {
+                        $('body').trigger(self.options.processStart);
+                    }
+                },
+                success: function(res) {
+                    if (self.isLoaderEnabled()) {
+                        $('body').trigger(self.options.processStop);
+                    }
+                    $(self.options.messagesSelector).addClass('added-to-cart');
+                    if (res.minicart) {
+                        $(self.options.minicartSelector).replaceWith(res.minicart);
+                        $(self.options.minicartSelector).trigger('contentUpdated');
+                    }
+                    if (res.product && res.product.statusText) {
+                        $(self.options.productStatusSelector)
+                            .removeClass('available')
+                            .addClass('unavailable')
+                            .find('span')
+                            .html(res.product.statusText);
+                    }
+                    self.enableAddToCartButton(form);
+                    if ($('.lookbook .product-item:not(.added-to-cart)').length) {
+                        $('.lookbook .product-item:not(.added-to-cart, .active)')[0].click();
+                    }
+                }
+            });
+        }
+    });
+
+    return $.MagentoEse.lookbookAddToCart;
+});
